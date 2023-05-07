bfdeded5a20836c352351858b6629f003e9edaaa
derleymad <filho.wanderley@hotmail.com>
2023-05-07 16:04:34 -0300

    adding images and nice lighting

--- README.md
+++ README.md
@@ -10,10 +10,16 @@
 ### ✨ Demo
 <p float="left">
   <img src="images/main.png" width="200" />
+    <img src="images/main2.png" width="200" />
 </p>
 
 <h2 align="center">View Port Scene - Parte 2 </h1>
-> TODO
+
+### ✨ Demo
+<p float="left">
+
+    <img src="images/main2.png" width="200" />
+</p>
 
 ## Como testar?
 
--- opengl-viewport/main.c
+++ opengl-viewport/main.c
@@ -1,121 +1,144 @@
 #include <GL/glut.h>
 
 // ----------- variaveis para controlar o angulo de cada bule --------------
-float angle = 0.0f;
-float angleXY = 0.0f;
-float angleXZ = 0.0f;
-float angleYZ = 0.0f;
+int width = 400;
+int height = 400;
+float angle = 0.0f;  // Ângulo de rotação do bule
 
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_COLOR_MATERIAL);
+void displayXY()
+{
+    glViewport(0, 200, 200, 200);
 
-//------------ iluminação -------------------------------------------------
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    //posicao da lus (tipo um ponto de iluminacao com a vela)
-    GLfloat lightPosition[] = { 0.0, 1.0, 0.0, 0.0 };
-    //luz global (tipo o brilho da tela)
-    GLfloat ambientLight[] = { 0.1, 0.1, 0.1, 1.0 };
-    //reflexao dos objetos (difusa)
-    GLfloat diffuseLight[] = { 1.0, 1.0, 1.0, 1.0 };
-    //para deixar liso e polido
-    GLfloat specularLight[] = { 0.0, 1.0, 0.0, 1.0 };
-
-    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
-
-    //perspectiva
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0f, 1.0f, 1.0f, 100.0f);
-
-    // camera para viewport perspectiva
-    glViewport(glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2, glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT));
-        glMatrixMode(GL_MODELVIEW);
+    glOrtho(-15.0, 15.0, -15.0, 15.0, -20.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(5.0f, 5.0f, 5.0f,
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 0.65f, 0.0f); //red
+    glutSolidTeapot(5.0f);
+}
+
+void displayYZ()
+{
+    glViewport(200, 200, 200, 200);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-15.0, 15.0, -15.0, 15.0, -20.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 0.65f, 0.0f); //segundo
+    glutSolidTeapot(5.0f);
+}
+
+void displayXZ()
+{
+    glViewport(0, 0, 200, 200);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-15.0, 15.0, -15.0, 15.0, -20.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0f, 15.0f, 0.0f,
+              0.0f, 0.0f, 0.0f,
+              0.0f, 0.0f, -1.0f); //vetor "up" apontando para baixo para ver de cima
+    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glColor3f(1.0f, 0.65f, 0.0f);//terceiro
+    glutSolidTeapot(5.0f);
+}
+
+
+void displayPerspective()
+{
+    glViewport(200, 0, 200, 200);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-15.0, 15.0, -15.0, 15.0, -20.0, 20.0);
+
+    //gluPerspective(100.0f, 1.0f, 1.0f, 10.0f); //n muda isso
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0f, 2.0f, 5.0f,
               0.0f, 0.0f, 0.0f,
               0.0f, 1.0f, 0.0f);
 
-    //desenha bule na perspectiva
-    glPushMatrix();
-    glTranslatef(0.0f, 0.5f, 0.0f);
     glRotatef(angle, 0.0f, 1.0f, 0.0f);
-    glColor3f(1.0f, 0.0f, 0.0f);  // Cor vermelha
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+    glColor3f(1.0f, 0.65f, 0.0f); //perspectiva
+    glutSolidTeapot(5.0f);
+}
+
+void enableLight()
+{
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    GLfloat mat_diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};    // Reflexão difusa
+    GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};   // Reflexão especular
+    GLfloat mat_shininess[] = {100.0f};                  // Brilho especular
+
+    GLfloat light_ambient[] = {0.2f, 0.2f, 0.2f, 1.0f};   // Luz ambiente
+    GLfloat light_diffuse[] = {1.f, 1.0f, 1.0f, 1.0f};   // Luz difusa
+    GLfloat light_specular[] = {1.0f, 1.f, 1.0f, 1.0f};  // Luz especular
+    GLfloat light_position[] = {1.0f, 1.0f, 1.0f, 0.0f};  // Posição da luz
+
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
 
-    //projecao
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-5.0f, 5.0f, -5.0f, 5.0f, -10.0f, 10.0f);    //projecao ortogonal
+    // Habilitar a iluminação
+    glEnable(GL_LIGHTING);
+}
 
-    // outras viewports
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_COLOR_MATERIAL);
 
-    //viewport XY
-    glViewport(0, glutGet(GLUT_WINDOW_HEIGHT) / 2, glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2);
-    glPushMatrix();
-    glRotatef(angleXY, 0.0f, 1.0f, 0.0f);  //eixo y
-    glColor3f(0.0f, 1.0f, 0.0f);  // verde
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+    enableLight(); // e se fez a luz
 
-    // viewport XZ
-    glViewport(glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2, glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2);
-    glPushMatrix();
-    glRotatef(angleXZ, 0.0f, 1.0f, 0.0f);
-    glColor3f(0.0f, 0.0f, 1.0f);  // azul
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+    //desenha os bules em cada perspectiva
+    //drawViewportBorders();
+    displayXY();
+    displayXZ();
+    displayYZ();
 
-    //viewport YZ
-    glViewport(0, 0, glutGet(GLUT_WINDOW_WIDTH) / 2, glutGet(GLUT_WINDOW_HEIGHT) / 2);
-    glPushMatrix();
-    glRotatef(angleYZ, 0.0f, 1.0f, 0.0f);  // eixo z
-    glColor3f(1.0f, 1.0f, 0.0f);  //amarelo
-    glutSolidTeapot(1.0f);
-    glPopMatrix();
+    displayPerspective(); //o capeta
 
-    // atualiza cada angulo para cada bule (ver funcao update)
-    angle += 0.5f;  // viewport perspectiva
-    angleXY += 1.0f;  //  viewport XY
-    angleXZ += 1.5f;  // viewport XZ
-    angleYZ += 2.0f;  //viewport YZ
-
-    // Trocar os buffers para exibir a cena
     glutSwapBuffers();
 }
 
 
-// funcao para atualizar valores (para animação)
-void update(int value) {
-
-    angle += 0.5f;
-    angleXY += 1.0f;
-    angleXZ += 1.5f;
-    angleYZ += 2.0f;
+void update(int value)
+{
+    angle += 1.0f;
+    if (angle > 360.0f)
+    {
+        angle -= 360.0f;
+    }
 
     glutPostRedisplay();
-
-    //glutTimerFunc(16, timerFunc, 0);  // 16 ms -> 60 fps (se for usar diminuir a incrementacao do aungulo pq fica mt rapido)
-
-    // (30 FPS)
-    glutTimerFunc(1000 / 30, update, 0);
+    glutTimerFunc(16, update, 0);
 }
 
-int main(int argc, char** argv) {
+
+// funcao para atualizar valores (para animação)
+
+
+int main(int argc, char** argv)
+{
     // criacao da janela
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(800, 600);
+    glutInitWindowSize(400, 400);
     glutCreateWindow("Renderização com Viewports");
 
     glutDisplayFunc(display);
@@ -129,4 +152,4 @@ int main(int argc, char** argv) {
 
     return 1;
 
-    }
+}
